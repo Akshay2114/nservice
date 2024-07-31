2e77403c76338f32d8e07588f77545726a87fd9b
Singh <2142714@cognizant.com>
2024-07-31 14:10:40 +0530

    Initial Commit

---
+++ src/main/java/com/example/nservice/NserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.nservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nservice/NserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
